86064cb5f8a2097cd1a2513c6bd8ca0813611e00
Sergey Nikiforov <serge.a.nikiforov@gmail.com>
2017-04-14 13:30:30 +0300

    update email settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -259,7 +259,7 @@ ACCOUNT_ALLOW_REGISTRATION = env.bool('DJANGO_ACCOUNT_ALLOW_REGISTRATION', True)
 # Custom user app defaults
 # Select the correct user model
 AUTH_USER_MODEL = 'users.User'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/api/'
 LOGIN_URL = 'account_login'
 
 # SLUGLIFIER
